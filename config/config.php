a3f106b6dd666897dd9e10deee40746e3ced1a81
Ildemaro Rodríguez <ildemaro88@gmail.com>
2018-01-07 11:30:15 -0400

    no se que cambios existan

--- config/config.php
+++ config/config.php
@@ -2,7 +2,7 @@
 
 define('DB_HOST','localhost'); 
 define('DB_USER','root'); 
-define('DB_PASS',''); 
+define('DB_PASS','1234567'); 
 define('DB_NAME','tricolor'); 
 define('DB_CHARSET','utf8');
 define('PATH_BACKUP', 'C:/wamp/www/proyecto3/backup/'); 
